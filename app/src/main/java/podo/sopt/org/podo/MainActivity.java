75d8b9ea12d8489b14fe1e1ef550a0158b28e2dd
Taehoon <noep@naver.com>
2016-05-14 09:10:19 +0900

    Add butterknife

--- app/src/main/java/podo/sopt/org/podo/MainActivity.java
+++ app/src/main/java/podo/sopt/org/podo/MainActivity.java
@@ -2,12 +2,31 @@ package podo.sopt.org.podo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.button) Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
+
+
+    @OnClick(R.id.button)
+    public void onClickTest(){
+        Toast.makeText(getApplicationContext(),"tttest",Toast.LENGTH_SHORT).show();
+    }
+
 }
